7780d49272605e2868d6a79e1bfb22a6c0ce3da7
dimacros <dimacros@hotmail.com>
2018-12-27 08:42:42 -0500

    Hacer nuevos cambios

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createAdmin();
+    }
+
+    private function createAdmin() 
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@dimacros.net',
+            'email_verified_at' => now(),
+            'password' => bcrypt('123456'),
+            'remember_token' => NULL
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false, 'reset' => false, 'verify' => true]);
-
+Auth::routes(['register' => false, 'reset' => true, 'verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/test', function(){
+
+    $service = new Peru\Sunat\Ruc();
+    $service->setClient(new Peru\Http\ContextClient());
+    $ruc = '10762119220';
+    $company = $service->get($ruc);
+    if ($company === false) {
+        return $service->getError();
+    }
+
+    return $company;
+
+});
