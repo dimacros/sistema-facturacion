b1a886c783488b47a26c5ab69bbe8f77809f404e
dimacros <31266588+dimacros@users.noreply.github.com>
2020-01-24 02:43:59 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false, 'reset' => true, 'verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/ruc/{ruc}', function($ruc){
+Route::get('/ruc/{ruc}', function($ruc) {
 
     $service = new Peru\Sunat\Ruc();
     $service->setClient(new Peru\Http\ContextClient());
@@ -28,22 +28,4 @@ Route::get('/ruc/{ruc}', function($ruc){
 
     return response()->json($company);
 
-});
-
-Route::get('/test', function(){
-    $params = request()->all();
-    $data = [];
-    for ($i=$params['offset']; $i < ($params['offset'] + $params['limit']); $i++) { 
-        $item = $i + 1;
-        $data[] = [
-            'id' => $item,
-            'name' => 'Item ' . $item, 
-            'price' => '$' . $item
-        ];
-    }
-    
-    return [
-        'total' => 1000,
-        'rows' => $data
-    ];
- });
+})->middleware('auth');
